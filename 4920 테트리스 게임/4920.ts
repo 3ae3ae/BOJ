8cb87468c311ebd91086d097d6eb7d74dbfd37d3
3ae3ae <ogyshjsm@naver.com>
2023-05-30 17:23:09 +0900

    기본 틀 만듦

--- 4920 테트리스 게임/4920.ts
+++ 4920 테트리스 게임/4920.ts
@@ -1,3 +1,11 @@
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const input = `4 
+70  2  1 7
+ 7  1 30 6 
+ 4 30 30 5 
+ 3  1 30 2 
+0`;
+
 function solution(input: string) {
   const Tetro = {
     L: [
@@ -43,29 +51,46 @@ function solution(input: string) {
   };
 
   const copy = <T>(a: T) => JSON.parse(JSON.stringify(a)) as T;
-  function rotate(T: number[][]) {
-    const newT = copy(T);
-    for (let x = 0; x < T[0].length; x++) {
-      for (let y = 0; y < T.length; y++) {
-        newT[-y + 3][x] = T[x][y];
+
+  (function makeShape() {
+    function rotate(T: number[][]) {
+      const newT = copy(T);
+      for (let x = 0; x < T[0].length; x++) {
+        for (let y = 0; y < T.length; y++) {
+          newT[-y + 3][x] = T[x][y];
+        }
       }
+      return newT;
     }
-    return newT;
-  }
 
-  function cut(T: number[][]) {
-    while (T[T.length - 1].every((x) => x === 0)) T.pop();
-    while (T[0].every((x) => x === 0)) T.shift();
-    while (T.map((x) => x[0]).every((x) => x === 0))
-      T.forEach((x) => x.shift());
-    while (T.map((x) => x[x.length - 1]).every((x) => x === 0))
-      T.forEach((x) => x.pop());
-  }
+    function cut(T: number[][]) {
+      while (T[T.length - 1].every((x) => x === 0)) T.pop();
+      while (T[0].every((x) => x === 0)) T.shift();
+      while (T.map((x) => x[0]).every((x) => x === 0))
+        T.forEach((x) => x.shift());
+      while (T.map((x) => x[x.length - 1]).every((x) => x === 0))
+        T.forEach((x) => x.pop());
+    }
+    [...Object.keys(Tetro)].forEach((key: keyof typeof Tetro) => {
+      for (let i = 0; i < 3; i++)
+        Tetro[key].push(rotate(Tetro[key][Tetro[key].length - 1]));
+      for (let i = 0; i < 4; i++) Tetro[key].forEach((x) => cut(x));
+    });
+  })();
 
-  [...Object.keys(Tetro)].forEach((key: keyof typeof Tetro) => {
-    for (let i = 0; i < 3; i++)
-      Tetro[key].push(rotate(Tetro[key][Tetro[key].length - 1]));
-    for (let i = 0; i < 4; i++) Tetro[key].forEach((x) => cut(x));
-  });
+  type gridType = number[][];
+  type gridsType = gridType[];
+
+  const grids: gridsType = [];
+  input
+    .split("\n")
+    .slice(0, -1)
+    .forEach((v) => {
+      const row = v.match(/-*\d+/g)?.map((x) => Number(x)) as number[];
+      if (row.length === 1) grids.push([]);
+      else grids[grids.length - 1].push(row);
+    });
+  function solve(grid: gridType): string {}
+  return grids.map((grid, i) => `${i + 1}. ${solve(grid)}`).join("\n");
 }
-solution("");
+console.log(solution(input));
