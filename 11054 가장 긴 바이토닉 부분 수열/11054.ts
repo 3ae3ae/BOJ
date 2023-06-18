5636c1e4e00c52a489cf84d4c073a07070e7d28c
3ae3ae <83870598+3ae3ae@users.noreply.github.com>
2023-06-18 10:20:56 +0000

    커밋 실수 수정

--- 11054 가장 긴 바이토닉 부분 수열/11054.ts
+++ 11054 가장 긴 바이토닉 부분 수열/11054.ts
@@ -1,19 +1,57 @@
 import fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim() as string;
+const input = fs.readFileSync("./dev/stdin").toString().trim();
 
-let [a, b] = input.split(" ").map((x) => BigInt(x));
 
-let result = 1;
-
-for (; a < b; result++) {
-  if (b % BigInt(10) < 2 && b% BigInt(10) > 0) {
-    b /= BigInt(10);
-  } else if (b % BigInt(2) < 1) {
-    b /= BigInt(2);
-  } else {
-    result = -1;
-    break;
+function solution(input: string): number {
+  const getMiddle = (l: number, r: number): number => Math.floor((l + r) / 2);
+  const sequence = input
+    .split("\n")[1]
+    .split(" ")
+    .map((x) => Number(x));
+  
+  function findOneWay(start:number, end:number, seq: number[], decrease = false):[number, number] {
+    /**
+     * n이 seq에서 몇번째 위치에 들어가야할 지 반환하는 함수
+    */
+    function binarySearch(seq: number[], n: number) {
+      let [l, r] = [0, seq.length - 1];
+      let m = getMiddle(l, r);
+      for (; l <= r; m = getMiddle(l, r)) {
+        if (n === seq[m]) return m;
+        if (n < seq[m]) r = m - 1;
+        else l = m + 1;
+      }
+      if (seq[m] < n) return m + 1;
+      else return m - 1;
+    }
+    const b: number[] = [0];
+    let max = 0;
+    const cdt = (i: number, start: number, end: number) => decrease ? i>=start : i<end;
+    for (let i = decrease ? end-1 : start; cdt(i, start, end); decrease ? i-- : i++) {
+      const p = binarySearch(b, seq[i]);
+      if (b[p] && b[p] <= seq[i]) continue;
+      else {
+        if (b[p]===undefined) max = seq[i];
+        b[p] = seq[i];
+      }
+    }
+    return [b.length-1, max];
   }
+
+  const increasing = findOneWay(0, sequence.length, sequence)[0];
+  const decreasing = findOneWay(0, sequence.length, sequence, true)[0];
+
+  let max = Math.max(increasing, decreasing);
+  for (let i=0; i<sequence.length; i++){
+    const [inc, ic] = findOneWay(0, i+1, sequence);
+    const [dec, dc] = findOneWay(i, sequence.length, sequence, true);
+    let tempmax = inc+dec;
+    if (ic===dc) tempmax--;
+    max = Math.max(max, tempmax);
+  }
+
+  return max;
+
+  
 }
-if (a>b) console.log(-1)
-else console.log(result);
+console.log(solution(input));
--- 16953 A → B/16953.ts
+++ 16953 A → B/16953.ts
@@ -5,16 +5,15 @@ let [a, b] = input.split(" ").map((x) => BigInt(x));
 
 let result = 1;
 
-for (; a < b || a > b; result++) {
+for (; a < b; result++) {
   if (b % BigInt(10) < 2 && b% BigInt(10) > 0) {
-    b %= BigInt(10);
+    b /= BigInt(10);
   } else if (b % BigInt(2) < 1) {
     b /= BigInt(2);
-  } else if (a >= b && a <= b) {
-    break;
   } else {
     result = -1;
     break;
   }
 }
-console.log(result);
+if (a>b) console.log(-1)
+else console.log(result);
